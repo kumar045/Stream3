a7554d2c90189a42236699dd336809e7cec60b23
Deepali <dipalij@skillup.tech>
2022-02-02 15:38:49 +0530

    make it better0904

--- stream_api/serializers.py
+++ stream_api/serializers.py
@@ -1,10 +1,16 @@
+import base64
+
+from django.core.files.base import ContentFile
 from rest_framework import serializers
-from drf_extra_fields.fields import Base64ImageField
 
 
-class StreamSerializer(serializers.Serializer):
-    image=Base64ImageField(required=False) # From DRF Extra Fields
-    
-    
-   
+class Base64ImageField(serializers.ImageField):
+    def from_native(self, data):
+        if isinstance(data, basestring) and data.startswith('data:image'):
+            # base64 encoded image - decode
+            format, imgstr = data.split(';base64,')  # format ~= data:image/X,
+            ext = format.split('/')[-1]  # guess file extension
 
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+
+        return super(Base64ImageField, self).from_native(data)
